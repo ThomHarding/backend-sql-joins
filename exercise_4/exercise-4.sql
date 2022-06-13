e680bfc0ef731ec8e084131b32745a965bc3f558
THOMAS HARDING <hardingt604@gmail.com>
2022-06-13 15:57:42 -0600

    exercise test 4 passes

--- exercise_4/exercise-4.sql
+++ exercise_4/exercise-4.sql
@@ -1,2 +1,6 @@
 -- find all customers first_name, last_name, address, and city
 -- Similar to exercise 3 but with customer, address, city
+
+ SELECT cu.first_name, cu.last_name, a.address, ci.city
+ FROM customer cu LEFT JOIN address a ON cu.address_id = a.address_id
+ LEFT JOIN city ci ON ci.city_id = a.city_id
