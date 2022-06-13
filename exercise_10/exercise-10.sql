0e60f3aebf713bbd001cf3a7ff5eaa4e11b6b512
THOMAS HARDING <hardingt604@gmail.com>
2022-06-13 17:13:49 -0600

    exercises 9 and 10 are there but i'm not supposed to be doing this yet wahoo

--- exercise_10/exercise-10.sql
+++ exercise_10/exercise-10.sql
@@ -1,2 +1,9 @@
 -- find the category name of all inventory items that have never been rented
 -- Similar to exercise 9 with an additonal join
+
+ SELECT c.name
+ FROM film f LEFT JOIN inventory i ON i.film_id = f.film_id
+ LEFT JOIN rental r ON i.inventory_id = r.inventory_id
+ LEFT JOIN film_category fc ON f.film_id = fc.film_id
+ LEFT JOIN category c ON fc.category_id = c.category_id
+ WHERE r.rental_id IS NULL
--- exercise_9/exercise-9.sql
+++ exercise_9/exercise-9.sql
@@ -1,3 +1,8 @@
 -- find the film title of all inventory items that have never been rented
 -- LEFT JOIN FROM inventory to film and to rental
 -- use WHERE IS NULL
+
+ SELECT f.title
+ FROM film f LEFT JOIN inventory i ON i.film_id = f.film_id
+ LEFT JOIN rental r ON i.inventory_id = r.inventory_id
+ WHERE r.rental_id IS NULL
