2abc2b6a7adb84fe025821aff0f5bf36303244f0
THOMAS HARDING <hardingt604@gmail.com>
2022-06-13 16:05:53 -0600

    exercise test 6 passes

--- exercise_6/exercise-6.sql
+++ exercise_6/exercise-6.sql
@@ -1,2 +1,8 @@
 -- find all film titles rented out by customers with the first_name Roberta
 -- Similar to exercise 5
+
+ SELECT f.title
+ FROM film f LEFT JOIN inventory i ON i.film_id = f.film_id
+ LEFT JOIN rental r ON i.inventory_id = r.inventory_id
+ LEFT JOIN customer c ON r.customer_id = c.customer_id
+ WHERE c.first_name = 'Roberta'
