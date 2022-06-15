39aa53134fa1b96c99a2c0b16c793dfe594f0268
THOMAS HARDING <hardingt604@gmail.com>
2022-06-15 13:17:54 -0600

    test 11 passes

--- exercise_11/exercise_11.sql
+++ exercise_11/exercise_11.sql
@@ -4,9 +4,10 @@
 -- ORDER in descending order
 -- LIMIT 10
  
- SELECT f.title, COUNT(f.film_id) as count
- FROM inventory i INNER JOIN film f ON f.film_id = i.film_id
- INNER JOIN rental r ON i.inventory_id = r.inventory_id
- GROUP BY f.title
+ SELECT f.title, COUNT(f.title) as count
+ FROM inventory i LEFT JOIN film f ON f.film_id = i.film_id
+ LEFT JOIN rental r ON i.inventory_id = r.inventory_id
+ GROUP BY f.film_id
  ORDER BY count desc
  LIMIT 10
+ 
